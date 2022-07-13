8d8cbeacee0e2408487c98de05ed2d69a12659cd
Laurence Barker <laurence@nicklebyhouse.co.uk>
2022-07-13 20:59:27 +0100

    rewrote axi_spi_adc (correct file this time)

--- FPGA/sources/verilogmodules/axi_spi_adc.v
+++ FPGA/sources/verilogmodules/axi_spi_adc.v
@@ -6,16 +6,14 @@
 // Create Date: 15.03.2021 17:19:26
 // Design Name: ADC78H90 ADC reader
 // Module Name: AXI_SPI_ADC
-// Project Name: Pluto
+// Project Name: Saturn
 // Target Devices: Artix 7
 // Tool Versions: Vivado
 // Description: Reader for ADC78H90. Reads data into registers; peah hold for
 //              forward/reverse power; axi4 read port.
 //              clocked by AXI clock, not ADC clock
 //
-// ADC reading code based on HPSDR code by Phil Harman VK6APH
-// ADC reading part Copyright 2007, 2008, 2009 Phil Harman VK6APH
-// AXI readout pary Copyright 2021 Laurence Barker G8NJJ
+// Copyright 2022 Laurence Barker G8NJJ
 // 
 // Dependencies: 
 // 
@@ -69,11 +67,11 @@ module AXI_SPI_ADC #
 // axi interface
   reg [AXI_ADDR_WIDTH-1:0] raddrreg;
   reg [AXI_DATA_WIDTH-1:0] rdatareg;
-  reg arreadyreg;                           // false when write address has been latched
-  reg rvalidreg;                            // true when read data out is valid
+  reg arreadyreg;                          // false when write address has been latched
+  reg rvalidreg;                           // true when read data out is valid
 
-  reg[1:0] clk_divide;				// derive input clock/4 (31.25MHz)
-  reg[1:0] clk_phase;				// derive input clock/16 as 4 phases (7.8125MHz)
+  reg[1:0] clk_divide;				       // derive input clock/4 (31.25MHz)
+  reg[1:0] clk_phase;				       // derive input clock/16 as 4 phases (7.8125MHz)
 
 // internal storage registers for ADC results
   reg [11:0] AIN1; // 
@@ -84,11 +82,11 @@ module AXI_SPI_ADC #
   reg [11:0] AIN6; // holds 13.8v supply voltage
 
 // internal registers for SPI shift
-  reg  [2:0] ADCAddress;			// current register address
-  reg  [2:0] NextADCAddress;			// next ADC address			
-  reg  [15:0] ADCData;				// shifted data
-  reg   [4:0] BitCnt;				// bit counter in shift sequence
-
+  reg  [2:0] ADCAddress;			        // current register address
+  reg  [2:0] NextADCAddress;                // next ADC address			
+  reg  [15:0] ADCData;				        // shifted data
+  reg   [4:0] BitCnt;				        // bit counter in shift sequence
+ 
 // internal registers. When set, clear AIN1 or AIN2
   reg clear_AIN1;                       // asserted by AXI read block to clear a peak hold reg when next accessed
   reg clear_AIN2;
@@ -96,19 +94,28 @@ module AXI_SPI_ADC #
   reg release_clear_AIN2;
 
 //
-// generate 31.25MHz clock
+// generate 31.25MHz clock and 7.8125MHz clock
 //
   always @ (posedge aclk)
   if(~aresetn)
   begin
     clk_divide = 2'b00;                 // reset to 0
+    clk_phase = 2'b00;                  // reset to 0
   end
   else
   begin
     if(clk_divide == 2'b11)             // if count =3, reset to 0
     begin
       clk_divide <= 2'b00;
-
+      // move clock phase to next state
+      if(clk_phase == 2'b11)
+      begin
+        clk_phase <= 2'b00;
+      end
+      else
+      begin
+        clk_phase <= (clk_phase + 1);   // else increment count
+      end
     end
     else
     begin
@@ -122,10 +129,10 @@ module AXI_SPI_ADC #
   always @ (posedge aclk)
   if(~aresetn)
   begin
-    ADC_state <= 3'b000;                // reset to 0
-    bit_cnt <= 3'b000;                  // reset to 0
-	ADC_address <= 16'b0;		        // reset ADC address
-	ADC_data <= 16'b0;                  // shift register
+    BitCnt <= 5'b000;                   // reset to 0
+	ADCAddress <= 3'b101;		        // reset current ADC address
+	NextADCAddress <= 3'b000;		    // reset ADC address
+	ADCData <= 16'b0;                   // shift register
 	release_clear_AIN1 <= 0;            // don't clear
 	release_clear_AIN2 <= 0;
 	AIN1 <= 0;
@@ -134,9 +141,10 @@ module AXI_SPI_ADC #
 	AIN4 <= 0;
 	AIN5 <= 0;
 	AIN6 <= 0;
-	
   end
+  //
   // do erase on alternate ticks (every 4th clock, but out of phase with main code)
+  //
   else if (clk_divide == 2'b10)         // else every 4th clock
   begin
     if(clear_AIN1)
@@ -156,70 +164,76 @@ module AXI_SPI_ADC #
       release_clear_AIN1 <= 0;
     if(release_clear_AIN2 == 1)
       release_clear_AIN2 <= 0;
-  
   end
+  //
   // main shifting code
+  //
   else if (clk_divide == 2'b00)         // else every 4th clock
   begin
-    case (ADC_state)
-    0:                                  
-    // non shifting data state; save previously shifted data to a register
-  	  begin
-  	  // peak hold to be added
-    	case (ADC_address[13:11])          // save shifted data to register
-		  0: 	AIN6 <= ADC_data;   	// capture incoming data
-		  1:	if(ADC_data > AIN1)
-		          AIN1 <= ADC_data;        // peak hold, unless peak cleared
-		  2:	if(ADC_data > AIN2)
-		          AIN2 <= ADC_data;        // peak hold, unless peak cleared
-		  3:	AIN3 <= ADC_data;
-		  4:	AIN4 <= ADC_data;
-		  5:	AIN5 <= ADC_data;
-	//default: ADC_address = 0;
-	  endcase	
-  	  
-      nCS       <= 1'b1;          // set nCS high
-      bit_cnt   <= 4'd15;         // set bit counter
-      if (ADC_address[13:11] == 3'd5)
-		ADC_address <= 16'b0;		// reset ADC address
-	  else ADC_address <= ADC_address + 16'b0000_1000_0000_0000;  // increment ADC address
-      ADC_state <= 1;
-	  end
-	
-    1:
-	  begin
-      nCS       <= 0;             		    // select ADC
-      MOSI      <= ADC_address[bit_cnt];	// shift address out to MOSI
-      ADC_state <= 2;
-	  end
-	
-    2:
-	  begin
-      SCLK      <= 1'b1;                    // SCLK becomes high
-      ADC_state <= 3;
-	  end
-	
-    3:
-	  begin
-	  if(bit_cnt <= 11)
-	   ADC_data[bit_cnt] <= MISO;          // shift in ADC data
-      SCLK      <= 1'b0;                   // SCLK becomes low
-      ADC_state <= 4;
-	  end
-
-    4:
-	  begin
-	    if (bit_cnt == 0)                  // restart sequence, store shifted data
-		  ADC_state <= 0;
-	    else
-	    begin
-	      bit_cnt   <= bit_cnt - 1'b1;     // continue shifting data
-	      ADC_state <= 1;
-	    end
-	  end 
-	
-    default:
-      ADC_state <= 0;
+    case (clk_phase)
+    0:  begin
+            if(BitCnt == 5'b00000)               //clear data register
+            begin
+                ADCData <= 16'b0;                   // shift register
+                nCS <= 0;                           // asset chip select
+            end
+            else if(BitCnt == 5'b10000)             // deassert CS in last state
+            begin
+                nCS <= 1;
+                // now save ADC data to correct register
+                case (ADCAddress)          // save shifted data to register
+                    0:	if(ADCData > AIN1)
+                        AIN1 <= ADCData;        // peak hold, unless peak cleared
+                    1:	if(ADCData > AIN2)
+                        AIN2 <= ADCData;        // peak hold, unless peak cleared
+                    2: 	AIN3 <= ADCData;   	    // capture incoming data
+                    3:	AIN4 <= ADCData;
+                    4:	AIN5 <= ADCData;
+                    5:	AIN6 <= ADCData;
+                endcase	
+                
+            end
+        end                             
+    
+    1:  begin
+            SCLK <= 0;                  // clock toggle
+            if(BitCnt == 5'b00010)      // MISO 1st o/p bit
+                MOSI <= NextADCAddress[2];
+            else if(BitCnt == 5'b00011)      // MISO 2nd o/p bit
+                MOSI <= NextADCAddress[1];
+            else if(BitCnt == 5'b00100)      // MISO 3rd o/p bit
+                MOSI <= NextADCAddress[0];
+        end
+    
+    2:  begin
+            if(BitCnt == 16)            // increment ADC address
+            begin
+                ADCAddress <= NextADCAddress;
+                if(NextADCAddress >= 5)
+                    NextADCAddress <= 0;
+                else
+                    NextADCAddress <= NextADCAddress + 1;
+            end
+        end
+    
+    3:  begin
+            SCLK <= 1;                  // clock toggle
+            // shift data; only add MOSI after 4 zero bits
+            if(BitCnt <= 15)
+            begin
+                ADCData[15:1] <= ADCData[14:0];
+                if(BitCnt >= 4)
+                    ADCData[0] <= MISO;
+                else
+                    ADCData[0] <= 0;
+            end
+            
+            if(BitCnt != 5'b10000)      // advance bit count
+                BitCnt <= BitCnt+1;
+            else
+                BitCnt <= 0;    
+        end
+    
     endcase
   end 
 
@@ -274,7 +288,7 @@ module AXI_SPI_ADC #
           3: rdatareg <= AIN4;
           4: rdatareg <= AIN5;
           5: rdatareg <= AIN6;
-          default: rdatareg <= AIN1;
+          default: rdatareg <= 0;
         endcase
       end
 // step 4. When rvalid and rready, terminate the transaction & clear data. 
