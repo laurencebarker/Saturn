a53360ba26c0dbec9b880d26147e8d47f4516173
laurencebarker <laurence@nicklebyhouse.co.uk>
2023-07-01 19:43:12 +0100

    added device reset to flashwriter

--- sw_tools/flashwriter/flashwriter.cpp
+++ sw_tools/flashwriter/flashwriter.cpp
@@ -42,6 +42,7 @@ gboolean PrimaryImage;              // set true if primary image to be written
 
 #define VFALLBACKADDR 0x00000000    // start of flash Flash
 #define VPRIMARYADDR  0x00980000    // at end of 1st image + timer barrier
+#define VDEVICESIZE 0x02000000      // 32MByte
 //
 // SPI writer constron structure:
 //
@@ -120,6 +121,77 @@ static void MyStatusCallback(const pgm_status_s& stat)
     // update the window
     while(gtk_events_pending())
         gtk_main_iteration();
+}
+
+
+// called when "erase device" button is clicked
+void on_erase_button_clicked()
+{
+    gchar TempString[100];
+    uint32_t StartAddress;
+    uint32_t EraseSize;
+
+    if(DriverPresent)
+    {
+        cfg.BaseAddress = 0x10000;                // Base address of the SPI IP
+        cfg.HasFifos = 1;                         // Does device have FIFOs?
+        cfg.SlaveOnly = 0;                        // Is the device slave only?
+        cfg.NumSlaveBits = 1;                     // Num of slave select bits on the device
+        cfg.DataWidth = XSP_DATAWIDTH_BYTE;       // Data transfer Width. 0=byte
+        cfg.SpiMode = XSP_QUAD_MODE;              // Standard/Dual/Quad mode
+        cfg.AxiFullBaseAddress = 0x10000;         // AXI Full Interface Base address of the SPI IP (unused?)
+        cfg.XipMode = 0;                          // 0 if Non-XIP, 1 if XIP Mode
+        cfg.Use_Startup = 1;                      // 1 if Startup block is used in h/w
+        cfg.dev_fname = gAXI_FNAME;               // Default to XDMA driver, first device
+
+      // Unused properties
+        cfg.AxiInterface = 0;             // AXI-Lite/AXI Full Interface
+        cfg.DeviceId = 0;                 // Unique ID  of device
+
+        StartAddress = 0x0;
+        EraseSize = VDEVICESIZE;                   // 32MByte
+
+        gtk_text_buffer_insert_at_cursor(TextBuffer, "Erase Whole device:\n", -1);
+        // update the window
+        while(gtk_events_pending())
+            gtk_main_iteration();
+        // begin programming operation
+        try
+        {
+            // Instantiate flash programming class
+            SPI_S25FL_c fifc;
+            fifc.RegisterStatusCallback(MyStatusCallback);
+
+            // Init
+            gtk_text_buffer_insert_at_cursor(TextBuffer, "Initialising:\n", -1);
+            gtk_label_set_label(LblStage, "Initialise");
+            // update the window
+            while(gtk_events_pending())
+                gtk_main_iteration();
+            fifc.Init(cfg);
+
+            // Mark erase/program start
+            const auto elap_start = std::chrono::steady_clock::now();
+
+            // Erase
+            gtk_text_buffer_insert_at_cursor(TextBuffer, "Erasing: ", -1);
+            gtk_label_set_label(LblStage, "Erase");
+            // update the window
+            while(gtk_events_pending())
+                gtk_main_iteration();
+            auto start = std::chrono::steady_clock::now();
+            fifc.EraseRange(StartAddress, EraseSize);
+            std::chrono::duration<double> dt = std::chrono::steady_clock::now() - start;
+            sprintf(TempString, "complete in %.3fs...\n", dt.count());
+            gtk_text_buffer_insert_at_cursor(TextBuffer, TempString, -1);
+            gtk_label_set_label(LblStage, "Complete");
+        }
+        catch (const std::exception& ex)
+        {
+            sprintf(TempString, "\nException occurred: %s\n", ex.what());
+            gtk_text_buffer_insert_at_cursor(TextBuffer, TempString, -1);
+        }
+    }
 }
 
 
@@ -320,6 +392,7 @@ int main(int argc, char *argv[])
     RbPrimary = GTK_TOGGLE_BUTTON(gtk_builder_get_object(Builder, "rb_1"));
     RbFallback = GTK_TOGGLE_BUTTON(gtk_builder_get_object(Builder, "rb_2"));
 
+    gtk_builder_add_callback_symbol (Builder, "OnEraseButtonClicked", G_CALLBACK (on_erase_button_clicked));
     gtk_builder_add_callback_symbol (Builder, "on_program_button_clicked", G_CALLBACK (on_program_button_clicked));
     gtk_builder_add_callback_symbol (Builder, "on_file_button_clicked", G_CALLBACK (on_file_button_clicked));
     gtk_builder_add_callback_symbol (Builder, "on_window_main_destroy", G_CALLBACK (on_window_main_destroy));
