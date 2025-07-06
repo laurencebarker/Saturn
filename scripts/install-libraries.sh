614fb81e4692301485b550d9c8325e76f8c9ad59
Jerry DeLong <kd4yal@gmail.com>
2025-07-06 09:52:39 -0400

    Update install-libraries.sh
    Updated libraries required by piHPSDR and update-pihpsdr.py

--- scripts/install-libraries.sh
+++ scripts/install-libraries.sh
@@ -1,7 +1,19 @@
-#
-# script to install libraries required by desktop apps
-#
-echo "install libraries..."
-sudo apt-get install libgpiod-dev
-sudo apt-get install libi2c-dev
-echo "install complete"
+#!/bin/bash
+
+# Script to install 
+
+echo "Installing libraries..."
+sudo apt-get install -y libgpiod-dev
+sudo apt-get install -y libi2c-dev
+sudo apt-get install -y rsync
+sudo apt-get install -y lxterminal
+sudo apt-get install -y libglib2.0-bin
+if [ -f ~/venv/bin/activate ]; then
+    source ~/venv/bin/activate
+    pip install rich==14.0.0
+    pip install psutil==7.0.0
+else
+    echo "Error: Virtual environment not found at ~/venv"
+    exit 1
+fi
+echo "Install complete"
