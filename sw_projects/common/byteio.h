3563df8552581400d1cab5f3bc32e671ffaca4a5
laurencebarker <laurence@nicklebyhouse.co.uk>
2025-09-30 20:02:42 +0100

    formatting change so open and close brackets aligned

--- sw_projects/common/byteio.h
+++ sw_projects/common/byteio.h
@@ -19,48 +19,56 @@
  */
 
 /* Read big-endian values from potentially unaligned buffers */
-SATURN_INLINE uint16_t rd_be_u16(const void *p) {
+SATURN_INLINE uint16_t rd_be_u16(const void *p)
+{
     uint16_t v;
     memcpy(&v, p, sizeof(v));
     return ntohs(v);
 }
 
-SATURN_INLINE uint32_t rd_be_u32(const void *p) {
+SATURN_INLINE uint32_t rd_be_u32(const void *p)
+{
     uint32_t v;
     memcpy(&v, p, sizeof(v));
     return ntohl(v);
 }
 
 /* Read little-endian values from potentially unaligned buffers */
-SATURN_INLINE uint16_t rd_le_u16(const void *p) {
+SATURN_INLINE uint16_t rd_le_u16(const void *p) 
+{
     uint16_t v;
     memcpy(&v, p, sizeof(v));
     return v; // Already in host byte order
 }
 
-SATURN_INLINE uint32_t rd_le_u32(const void *p) {
+SATURN_INLINE uint32_t rd_le_u32(const void *p) 
+{
     uint32_t v;
     memcpy(&v, p, sizeof(v));
     return v; // Already in host byte order
 }
 
 /* Write big-endian values to potentially unaligned buffers */
-SATURN_INLINE void wr_be_u16(void *p, uint16_t val) {
+SATURN_INLINE void wr_be_u16(void *p, uint16_t val) 
+{
     uint16_t v = htons(val);
     memcpy(p, &v, sizeof(v));
 }
 
-SATURN_INLINE void wr_be_u32(void *p, uint32_t val) {
+SATURN_INLINE void wr_be_u32(void *p, uint32_t val) 
+{
     uint32_t v = htonl(val);
     memcpy(p, &v, sizeof(v));
 }
 
 /* Write little-endian values to potentially unaligned buffers */
-SATURN_INLINE void wr_le_u16(void *p, uint16_t val) {
+SATURN_INLINE void wr_le_u16(void *p, uint16_t val) 
+{
     memcpy(p, &val, sizeof(val));
 }
 
-SATURN_INLINE void wr_le_u32(void *p, uint32_t val) {
+SATURN_INLINE void wr_le_u32(void *p, uint32_t val) 
+{
     memcpy(p, &val, sizeof(val));
 }
 
