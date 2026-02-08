a99ece28aabefb76ffaade2c4fc31a1c3d8d0e33
laurencebarker <laurence@nicklebyhouse.co.uk>
2026-02-08 09:55:55 +0000

    changed shutdown-waiter.sh to not wait if G2V1 type panel fitted

--- scripts/shutdown-waiter.sh
+++ scripts/shutdown-waiter.sh
@@ -4,17 +4,26 @@
 # The first read will be before the pullup is activated
 # so wait for a delay before scanning proper. 
 #
-sleep 20
-gpioget --bias=pull-up gpiochip0 26
-pinvalue=1
-sleep 2
-echo "waiting for shutdown to be triggered..."
-while [ $pinvalue -eq 1 ]
-do
-# poll input every second, and wait for logic zero
-	pinvalue=$(gpioget --bias=pull-up gpiochip0 26)
-	sleep 1
-done
-echo "shutdown request detected"
-shutdown 0
+# this should only be needed on G2V2 vintage radios. 
+# Radios with a G2V1 panel use GPIO26 as an encodeer input.
+# so exit script if G2V1 I2C device is found.
+if i2cget -y 1 0x20
+then
+	echo "i2c device found, so shutdown waiter not needed"
+else
+	echo "G2V2 type radio found with no I2C device"
+	sleep 20
+	gpioget --bias=pull-up gpiochip0 26
+	pinvalue=1
+	sleep 2
+	echo "waiting for shutdown to be triggered..."
 
+	while [ $pinvalue -eq 1 ]
+	do
+# poll input every second, and wait for logic zero
+		pinvalue=$(gpioget --bias=pull-up gpiochip0 26)
+		sleep 1
+	done
+	echo "shutdown request detected"
+	shutdown 0
+fi
