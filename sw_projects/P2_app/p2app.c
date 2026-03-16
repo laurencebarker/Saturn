aa4aad199fa112518332769abbefa293b4db6541
laurencebarker <laurence@nicklebyhouse.co.uk>
2026-03-16 11:29:20 +0000

    updated p2app version to V45

--- sw_projects/P2_app/p2app.c
+++ sw_projects/P2_app/p2app.c
@@ -66,7 +66,7 @@
 #include "frontpanelhandler.h"
 #include "GanymedePAControl.h"
 
-#define P2APPVERSION 44
+#define P2APPVERSION 45
 #define FWREQUIREDMAJORVERSION 1                  // major version that is required. Only altered if programming interface changes. 
 //
 // the Firmware version is a protection to make sure that if a p2app update is required by the new firmware,
@@ -74,6 +74,7 @@
 //
 //------------------------------------------------------------------------------------------
 // VERSION History
+// V45, 16/3/2026.   encodes max ADC1, ADC2 sample in the message period into the high priority status message.
 // V44, 31/01/2026.  Support for Thetis "push" CAT commands for Ganymede, g2v2 indicators & Aries instead of polling
 // V43, 19/01/2026.  Initial support for Ganymede PA controller if stared with -g switch.
 // V42, 05/01/2026.  Support for new codec; debug mode with startup switch to enable interlinked DDC at different frequencies
