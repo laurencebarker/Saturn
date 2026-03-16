ac5f6dc3227043a510922356677668badb74fe8c
laurencebarker <laurence@nicklebyhouse.co.uk>
2026-03-16 14:05:24 +0000

    added script build-xdma to install xdma driver from scratch

---
+++ scripts/build-xdma.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# build-xdma.sh
+# Version: 1.0
+# Laurence Barker G8NJJ
+
+cd github/saturn/linuxdriver/xdma
+sudo apt install linux-headers-rpi-v8
+
+# build and install driver
+make
+sudo make install
+sudo modprobe xdma
+
+#install udev rules file
+sudo cp ../etc/udev/rules.d/* /etc/udev/rules.d
+
+echo please restart before driver is active
