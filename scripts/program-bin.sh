6df05acd3243b7e21aeec0533c0d355ab7028834
laurencebarker <laurence@nicklebyhouse.co.uk>
2026-03-21 13:04:33 +0000

    updated program-bin.sh script
    programs the FPGA with the newest FPGA binary file, if that is a newer
    versino than the current version

--- scripts/program-bin.sh
+++ scripts/program-bin.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 # find the latest FPGA flash file, and program it
+echo "finding current and avaiable FPGA versions:"
 
-# navigation step
+# navigate to FPGA folder:
 cd ~/github/Saturn/FPGA
 
-#Get the lastest file based on version number using sort and tail
+# get the latest file name based on version number using sort and tail
 latest_file=$(ls saturnprimary*.bin | sort -V | tail -n 1)
 
-#Output the latest file
-echo "The latest FPGA firmware image file is: $latest_file"
-echo "calling programmer app"
+# get the current version by reading back from FPGA:
+current_file=$(~/github/Saturn/sw_tools/FPGAVersion/FPGAVersion)
 
+# determine the version number by clipping the numerical parts:
+# remove first part of filename to get a string like "27.bin"
+available_vnumber_start=${latest_file:18}
+available_vnumber=$(echo "$available_vnumber_start" | tr -d -c 0-9)
+
+current_vnumber=${current_file:24}
+echo "comparing versions"
+echo "The available FPGA code: $available_vnumber"
+echo "The currently programmed FPGA version: $current_vnumber"
+
+if [[ "$available_vnumber" == "$current_vnumber" ]]
+then
+echo "FPGA version is already up to date"
+else
+echo "calling programmer app with file: $latest_file"
 ~/github/Saturn/sw_tools/load-FPGA/load-FPGA -b $latest_file -v
-#
 echo "FPGA programming complete. Please power off and back on"
+fi
+
+
 
 
 
---
+++ sw_tools/FPGAVersion/readme.md
@@ -0,0 +1,14 @@
+Command line FPGA flash version reader
+This simple app reads back the FPGA version, and displays it
+
+
+build instructions:
+
+make
+
+
+usage:
+./FPGAVersion
+
+Displays a simple string such as "FPGA Firmware version = 27"
+
